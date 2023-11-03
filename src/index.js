0eb3fff6fa72dd52c17b0a04a3918b1b73a14851
Sascha Holmes <146263983+gigipink1@users.noreply.github.com>
2023-11-03 15:43:13 +0000

    Add createFoodItem function to menu.js and set up examples in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './style.css'
 import {createHeader, createFooter} from './modules/base';
 import { createHero, createInfo } from './modules/home';
-import {createMenuLayout} from './modules/menu';
+import {createMenuLayout, createFoodItem} from './modules/menu';
 
 
 
@@ -9,4 +9,9 @@ import {createMenuLayout} from './modules/menu';
 
 createHeader();
 createMenuLayout();
+createFoodItem('Sandwiches','Lorem ipsum dolor sit amet, consectetur adipiscing elit','£3.50');
+createFoodItem('Panini', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', '£5.45');
+createFoodItem('Pasta Salad', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', '£5.25');
+createFoodItem('Full English', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', '£6.50');
+createFoodItem('Afternoon Tea', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', '£9.95')
 createFooter();
--- src/modules/menu.js
+++ src/modules/menu.js
@@ -3,13 +3,18 @@ function createMenuLayout() {
     const outerDiv = document.createElement('div');
     const div = document.createElement('div');
     const header = document.createElement('h2');
+    const listOne = document.createElement('ul');
+    const listTwo = document.createElement('ul');
     
+
     outerDiv.classList.add('menu-page');
     header.classList.add('food-header');
     header.textContent = 'Food';
     outerDiv.appendChild(header.cloneNode(1));
     div.classList.add('food-section');
-    
+    listOne.classList.add('food-list');
+   
+    div.appendChild(listOne);
     outerDiv.appendChild(div.cloneNode(1));
     
     header.classList.remove('food-header');
@@ -18,11 +23,28 @@ function createMenuLayout() {
     outerDiv.appendChild(header.cloneNode(1));
     div.classList.remove('food-section');
     div.classList.add('drinks-section');
-   
-   
+    div.removeChild(div.firstChild);
+    listTwo.classList.add('drinks-list');
+    div.appendChild(listTwo)
     outerDiv.appendChild(div);
     main.appendChild(outerDiv);
 
 }
 
-export {createMenuLayout};
+function createFoodItem(name, desc, price) {
+    const list = document.querySelector('.food-list');
+    const listItem = document.createElement('li');
+    const subList = document.createElement('ul');
+    listItem.textContent = desc;
+    subList.appendChild(listItem.cloneNode(1));
+    listItem.textContent = price;
+    subList.appendChild(listItem.cloneNode(1));
+    listItem.textContent = name;
+    listItem.classList.add('food-item')
+    listItem.appendChild(subList);
+    list.appendChild(listItem.cloneNode(1));
+
+
+}
+
+export {createMenuLayout, createFoodItem};
