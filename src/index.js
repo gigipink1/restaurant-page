d3523f7451c2ad715df393b22f297f361223b37c
Sascha Holmes <146263983+gigipink1@users.noreply.github.com>
2023-11-03 17:33:32 +0000

    Move menu item creation to menu file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import './style.css'
 import {createHeader, createFooter} from './modules/base';
 import { createHero, createInfo } from './modules/home';
-import {createMenuLayout, createFoodItem} from './modules/menu';
+import {createMenuLayout, addMenuItems} from './modules/menu';
 
 
 
 
 
 createHeader();
-createMenuLayout();
-createFoodItem('Sandwiches','Lorem ipsum dolor sit amet, consectetur adipiscing elit','£3.50');
-createFoodItem('Panini', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', '£5.45');
-createFoodItem('Pasta Salad', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', '£5.25');
-createFoodItem('Full English', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', '£6.50');
-createFoodItem('Afternoon Tea', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', '£9.95')
+
 createFooter();
--- src/modules/menu.js
+++ src/modules/menu.js
@@ -43,8 +43,41 @@ function createFoodItem(name, desc, price) {
     listItem.classList.add('food-item')
     listItem.appendChild(subList);
     list.appendChild(listItem.cloneNode(1));
+}
 
+function createDrinkItem(name, price) {
+    const list = document.querySelector('.drinks-list');
+    const listItem = document.createElement('li');
+    const subList = document.createElement('ul');
+    listItem.textContent = name;
+    subList.appendChild(listItem.cloneNode(1));
+    listItem.textContent = price;
+    subList.appendChild(listItem.cloneNode(1));
+    listItem.textContent = '';
+    listItem.classList.add('drinks-item');
+    listItem.appendChild(subList);
+    list.appendChild(listItem);
 
 }
 
-export {createMenuLayout, createFoodItem};
+function addMenuItems(){
+    createFoodItem('Sandwiches','Lorem ipsum dolor sit amet, consectetur adipiscing elit','£3.50');
+    createFoodItem('Panini', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', '£5.45');
+    createFoodItem('Pasta Salad', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', '£5.25');
+    createFoodItem('Full English', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', '£6.50');
+    createFoodItem('Afternoon Tea', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', '£9.95');
+    createDrinkItem('Latte', '£2.50');
+    createDrinkItem('Cappuccino', '£2.50');
+    createDrinkItem('Flat White', '£2.40');
+    createDrinkItem('Espresso', '£2.00');
+    createDrinkItem('Hot Chocolate', '£2.60');
+    createDrinkItem('Syrups', '+40p');
+    createDrinkItem('Milk Alternatives', 'Free')
+    createDrinkItem('Pot of Tea', '£2.20');
+    createDrinkItem('Soft Drinks', '£1.50');
+    createDrinkItem('Milkshakes', '£3.20');
+    createDrinkItem('Babyccinno', 'Free');
+
+}
+
+export {createMenuLayout, addMenuItems};
